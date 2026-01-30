d9b81f03d3cc9793caf5ea9284a49fd5183380f0
Chetan Tekam <tekamchetan3500@gmail.com>
2026-01-30 19:29:07 +0530

    feat: add Threshold of high traffic in analyze-logs.sh

--- scripts/analyze-logs.sh
+++ scripts/analyze-logs.sh
@@ -14,3 +14,18 @@ awk '{print $1}' $LOG | sort | uniq -c | sort -rn | head -5
 echo "--------------------------------------"
 echo "Total 404 Count : $(grep -c 404 $LOG)"
 echo "--------------------------------------"
+
+# Get the top talker (just the first line)
+top_entry=$(awk '{print $1}' /var/log/nginx/access.log | sort | uniq -c | sort -rn | head -1)
+
+# Split the entry into Count and IP
+count=$(echo $top_entry | awk '{print $1}')
+ip=$(echo $top_entry | awk '{print $2}')
+
+# The Threshold Logic
+THRESHOLD=50
+
+if [ "$count" -gt "$THRESHOLD" ]; then
+    sudo echo "$(date): ⚠️ ALERT! High traffic from $ip ($count requests)" >> /var/log/sre-alerts.log
+    # Pro SRE move: You could even trigger an iptables block here!
+fi
